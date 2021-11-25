0bd060b2d93b0fef464d9683b61e9493de0838c6
zzz664 <zzz664>
2021-11-25 21:33:32 +0900

    vue-form 최종 프로젝트 끝

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewString"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue';
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header',
+    }
+  },
   components: {
-    HelloWorld
-  }
+    'app-header': AppHeader,
+  },
+  methods: {
+    renewString: function() {
+      this.str = 'Hi';
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
